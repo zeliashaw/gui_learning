641d1728d762a6d59236fb38d2819a10e4167ba9
zeliashaw <shawze@wgc.school.nz>
2019-02-15 10:05:09 +1300

    update of hello_world adding labels

--- hello_world.py
+++ hello_world.py
@@ -4,4 +4,13 @@ root = tkinter.Tk()
 root.title("hello world")
 root.geometry("600x200")
 
+my_label = tkinter.Label(root)
+my_label.config(text="Purpelicious", fg="purple", font=("Times", "50", "bold"))
+my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="asdfghjkl", fg="blue", justify="left")
+my_label.grid()
+
+
 root.mainloop()
